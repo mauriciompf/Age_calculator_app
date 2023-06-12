9e2378f5688a8ca4bb2d361e23331e9ab823b6c8
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-06-12 19:42:22 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
-const inputNumbers = document.querySelectorAll("#digit");
+const inputYears = document.querySelector("#digit-years");
+const inputMonths = document.querySelector("#digit-months");
+const inputDays = document.querySelector("#digit-days");
 const digitYears = document.querySelector("#years");
 const digitMonths = document.querySelector("#months");
 const digitDays = document.querySelector("#days");
 
-const date = new Date();
-console.log(Math.abs(date.getFullYear() - 2002));
-// console.log(Math.abs(date.getMonth() - 12));
-// console.log(Math.abs(date.getDate() - 31));
+function calculateTimeDifference(birthdate) {
+  const birthdateObj = new Date(birthdate);
+  const currentDate = new Date();
+
+  const timeDiff = currentDate.getTime() - birthdateObj.getTime();
+  const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+  const years = Math.floor(daysDiff / 365);
+  const months = Math.floor((daysDiff % 365) / 30);
+  const days = (daysDiff % 365) % 30;
+
+  return { years, months, days };
+}
+
+const birthdate = `${inputYears.value}-${inputMonths.value}-${inputDays.value}`;
+const timeDifference = calculateTimeDifference(birthdate);
+
+inputYears.addEventListener("keyup", () => {
+  digitYears.textContent = inputYears.value;
+});
+
+console.log(
+  `${timeDifference.years}: years, ${timeDifference.months}: months, and ${timeDifference.days}: days`
+);
